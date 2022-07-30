3754458e600c85d3207415a49df76affb6b15103
Rajat <rajatrkgupta@gmail.com>
2022-07-31 00:17:40 +0530

    temp fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :cars
   root "parkings#index"
 
   resources :parkings do
     resources :entry_points, only: [:new, :edit, :create, :update]
     resources :spots, only: [:new, :edit, :create, :update]
   end
+
+  resources :cars
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_30_175243) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_30_184019) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "registration_number"
+    t.string "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "entry_points", force: :cascade do |t|
     t.string "number"
